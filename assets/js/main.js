95c2be4ded0e32b6c060e6216322378dbe5dc4bf
LucasRoberto99 <lucasr.prof@gmail.com>
2023-01-29 19:44:55 +0100

    saveDimancheSoir

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,28 +7,26 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelector("form-container").classList.toggle("invisible");
     });
 
-  document
-    .addEventListener("form")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
-      console.log("submit");
+  document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("submit");
 
-      const data = {
-        firstname: document.querySelector("#firstname").value,
-        lastname: document.querySelector("#lastname").value,
-        email: document.querySelector("#email").value,
-        message: document.querySelector("#message").value,
-      };
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      message: document.querySelector("#message").value,
+    };
 
-      console.log(data);
+    console.log(data);
 
-      // requete vers mon serveur
+    // requete vers mon serveur
 
-      const response = await axios.post(
-        "https://site--formulaireback--fhx5w78hhgzd.code.run/form",
-        data
-      );
+    const response = await axios.post(
+      "https://site--formulaireback--fhx5w78hhgzd.code.run/form",
+      data
+    );
 
-      console.log(response.data);
-    });
+    console.log(response.data);
+  });
 });
