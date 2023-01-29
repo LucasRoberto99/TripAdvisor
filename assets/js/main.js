c23f33986eaee1b914664d8704223831b4b57a7f
LucasRoberto99 <lucasr.prof@gmail.com>
2023-01-29 19:39:14 +0100

    saveDimancheSoir

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("content loaded");
+
+  document
+    .querySelector("#boutonConnectezVous")
+    .addEventListener("click", () => {
+      document.querySelector("form-container").classList.toggle("invisible");
+    });
+
+  document
+    .addEventListener("form")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      console.log("submit");
+
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      };
+
+      console.log(data);
+
+      // requete vers mon serveur
+
+      const response = await axios.post(
+        "https://site--formulaireback--fhx5w78hhgzd.code.run/form",
+        data
+      );
+
+      console.log(response.data);
+    });
+});
